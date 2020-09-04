bd93f8722ff8c324064b5cf40242b1ae2af67331
Carlos Fernando Villa Berrio <carlos.shizuken@gmail.com>
2020-09-03 21:15:05 -0500

    Septiembre 3 - 2020 FINAL
    ACTIVIDADES
    
    * (12:38 PM) Se investigo acerca de como incluir videos en los proyectos de Unity.
    
    * (12:38 PM) Se creo una breve explicación de como se juega el ajedrezM.
    
    * (12:38 PM) Se integro el video explicativo al menú del tutorial en la pantalla principal del proyecto de ajedrezM.
    
    * (9:04 PM) Se vio toda la tarde tutoriales de como usar videos dentro de Unity.
    
    * (9:04 PM) Se investigo como incluir controles para el video, ya que la función de video de Unity solo inicia el video sin poderse controlar In-Game.
    
    * (9:04 PM) Se logro adaptar una barra de progreso al video tutorial y los botones que permiten controlar la navegación del video.
    
    * (9:04 PM) Se sube pantallazo del logro adquirido.

---
+++ 003 AjedrezM/Assets/Scripts/VideoPlayer/BarraVideo.cs
@@ -0,0 +1,94 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Video;
+using UnityEngine.UI;
+
+public class BarraVideo : MonoBehaviour
+{
+    Slider barra;
+    public Button botonPlay;
+    public Button botonPause;
+    public VideoPlayer video;
+    public int seconds100xCiento;
+
+    public float max;
+    public float act;
+    public Text valorString;
+    int cont = 0;
+    string timerString;
+    int minutes;
+    int seconds;
+
+    private void Awake()
+    {
+        barra = GetComponent<Slider>();
+        botonPlay.gameObject.SetActive(false);
+    }
+
+    void Start()
+    {
+        FuncionPrueba();
+    }
+
+    public void ReiniciarVideo()
+    {
+        video.time = 0;
+    }
+    public void RetrocederVideo()
+    {
+        video.time -= 5;
+    }
+    public void AdelantarVideo()
+    {
+        video.time += 5;
+    }
+    public void VideoPlay()
+    {
+        if (video.isPlaying)
+        {
+            video.Pause();
+            botonPlay.gameObject.SetActive(false);
+            botonPause.gameObject.SetActive(true);
+        }
+    }
+    public void VideoPause()
+    {
+        if (!video.isPlaying)
+        {
+            video.Play();
+            botonPlay.gameObject.SetActive(true);
+            botonPause.gameObject.SetActive(false);
+        }
+    }
+    void ActualizarValorBarra(float valorMax, float valorAct)
+    {
+        double porcentaje;
+        //porcentaje = valorAct / valorMax;
+        porcentaje = video.clockTime;
+        barra.value = (float)porcentaje / seconds100xCiento;
+
+        seconds = (int)(porcentaje % 60);
+        minutes = (int)(porcentaje / 60);
+
+        timerString = string.Format("{00:00}:{01:00}", minutes, seconds);
+        //valorString.text = porcentaje * 100 + "%";
+        valorString.text = timerString;
+    }
+
+    void FuncionPrueba()
+    {
+        if(cont <= seconds100xCiento)
+        {
+            ActualizarValorBarra(seconds100xCiento, cont);
+            cont++;
+
+        }
+        else
+        {
+            cont = 0;
+        }
+        Invoke("FuncionPrueba", 1f);
+
+    }
+}
