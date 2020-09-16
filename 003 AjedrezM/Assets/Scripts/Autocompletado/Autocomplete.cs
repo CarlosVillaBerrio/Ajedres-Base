c2f2324e63691564b27a2caa809f45763eb4905f
Salomé Pelaez <spsalome@gmail.com>
2020-09-16 12:28:06 -0500

    IA primer nivel

--- 003 AjedrezM/Assets/Scripts/Autocompletado/Autocomplete.cs
+++ 003 AjedrezM/Assets/Scripts/Autocompletado/Autocomplete.cs
@@ -6,7 +6,7 @@ using UnityEngine;
 public class Autocomplete : MonoBehaviour
 {
     public GameObject horse;
-    public GameObject star1;
+    public GameObject figura;
     public GameObject particles;
     
     [SerializeField] private List<GameObject> points = new List<GameObject>();
@@ -20,7 +20,7 @@ public class Autocomplete : MonoBehaviour
             CancelInvoke();
         }
 
-        star1.SetActive(false);
+        figura.SetActive(false);
     }
 
     public void GetHorses()
@@ -28,13 +28,190 @@ public class Autocomplete : MonoBehaviour
         horse = GameObject.Find("Tablero/W Knight(Clone)");
     }
 
+    public void Cuadrado_8x8()
+    {        
+        StartCoroutine("CompleteFirstLevel");
+    }
+
     public void EstrellaCuatroPuntas_8x8()
     {        
         StartCoroutine("CompleteFifthLevel");
     }
 
+    #region Cuadrado, tablero 8x8
+    private IEnumerator CompleteFirstLevel() 
+    {
+        float offset = 0.5f;
+
+        Vector3 position = new Vector3(1f + offset, 0, 0f + offset);
+
+        horse.transform.position = position;
+
+        yield return new WaitForSeconds(1f);
+
+        position.x = 0f + offset;
+        position.z = 2f + offset;
+        
+        horse.transform.position = position;
+
+        yield return new WaitForSeconds(1f);
+
+        position.x = 2f + offset;
+        position.z = 4f + offset;
+        
+        horse.transform.position = position;
+
+        yield return new WaitForSeconds(1f);
+        
+        horse.transform.position = position;
+
+        position.x = 3f + offset;
+        position.z = 5f + offset;
+        
+        horse.transform.position = position;
+
+        yield return new WaitForSeconds(1f);
+
+        // Acá debe comenzar a dibujar la línea
+        position.x = 1f + offset;
+        position.z = 6f + offset;
+        
+        horse.transform.position = position;
+
+        GameObject t = Instantiate(particles, horse.transform.position, Quaternion.identity); 
+        points.Add(t);
+        t.transform.SetParent(transform);
+
+        yield return new WaitForSeconds(1f);
+
+        position.x = 3f + offset;
+        position.z = 5f + offset;
+        
+        horse.transform.position = position;
+        t = Instantiate(particles, horse.transform.position, Quaternion.identity); 
+        points.Add(t);
+        t.transform.SetParent(transform);
+
+        yield return new WaitForSeconds(1f);
+     
+        position.x = 4f + offset;
+        position.z = 7f + offset;
+        
+        horse.transform.position = position;
+        t = Instantiate(particles, horse.transform.position, Quaternion.identity); 
+        points.Add(t);
+        t.transform.SetParent(transform);
+
+        yield return new WaitForSeconds(1f);
+
+        position.x = 6f + offset;
+        position.z = 6f + offset;
+        
+        horse.transform.position = position;
+        t = Instantiate(particles, horse.transform.position, Quaternion.identity); 
+        points.Add(t);
+        t.transform.SetParent(transform);
+
+        yield return new WaitForSeconds(1f);
+
+        position.x = 4f + offset;
+        position.z = 5f + offset;
+        
+        horse.transform.position = position;
+        t = Instantiate(particles, horse.transform.position, Quaternion.identity); 
+        points.Add(t);
+        t.transform.SetParent(transform);
+
+        yield return new WaitForSeconds(1f);
+
+        position.x = 5f + offset;
+        position.z = 3f + offset;
+        
+        horse.transform.position = position;
+        t = Instantiate(particles, horse.transform.position, Quaternion.identity); 
+        points.Add(t);
+        t.transform.SetParent(transform);
+
+        yield return new WaitForSeconds(1f);
+
+        position.x = 6f + offset;
+        position.z = 1f + offset;
+        
+        horse.transform.position = position;
+        t = Instantiate(particles, horse.transform.position, Quaternion.identity); 
+        points.Add(t);
+        t.transform.SetParent(transform);
+
+        yield return new WaitForSeconds(1f);
+
+        position.x = 4f + offset;
+        position.z = 0f + offset;
+        
+        horse.transform.position = position;
+        t = Instantiate(particles, horse.transform.position, Quaternion.identity); 
+        points.Add(t);
+        t.transform.SetParent(transform);
+
+        yield return new WaitForSeconds(1f);
+
+        position.x = 3f + offset;
+        position.z = 2f + offset;
+        
+        horse.transform.position = position;
+        t = Instantiate(particles, horse.transform.position, Quaternion.identity); 
+        points.Add(t);
+        t.transform.SetParent(transform);
+
+        yield return new WaitForSeconds(1f);
+
+        position.x = 1f + offset;
+        position.z = 1f + offset;
+        
+        horse.transform.position = position;
+        t = Instantiate(particles, horse.transform.position, Quaternion.identity); 
+        points.Add(t);
+        t.transform.SetParent(transform);
+
+        yield return new WaitForSeconds(1f);
+
+        position.x = 0f + offset;
+        position.z = 3f + offset;
+        
+        horse.transform.position = position;
+        t = Instantiate(particles, horse.transform.position, Quaternion.identity); 
+        points.Add(t);
+        t.transform.SetParent(transform);
+
+        yield return new WaitForSeconds(1f);
+
+        position.x = 2f + offset;
+        position.z = 4f + offset;
+        
+        horse.transform.position = position;
+        t = Instantiate(particles, horse.transform.position, Quaternion.identity); 
+        points.Add(t);
+        t.transform.SetParent(transform);
+
+        yield return new WaitForSeconds(1f);
+
+        position.x = 1f + offset;
+        position.z = 6f + offset;
+        
+        horse.transform.position = position;
+
+        t = Instantiate(particles, horse.transform.position, Quaternion.identity); 
+        points.Add(t);
+        t.transform.SetParent(transform);
+
+        yield return new WaitForSeconds(0.3f);
+
+        figura.SetActive(true);
+    }
+    
+    #endregion
+
     #region Estrella de cuatro puntas, tablero 8x8
-    IEnumerator CompleteFifthLevel() 
+    private IEnumerator CompleteFifthLevel() 
     {
         float offset = 0.5f;
 
@@ -150,7 +327,7 @@ public class Autocomplete : MonoBehaviour
 
         yield return new WaitForSeconds(0.3f);
 
-        star1.SetActive(true);
+        figura.SetActive(true);
     }
     
     #endregion
